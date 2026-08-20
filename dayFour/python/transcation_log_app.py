19688c7565f792260ed9adba0e49280bf42c7746
panthera2020 <diranoye2020@gmail.com>
2026-08-20 13:51:12 +0100

    Trying somthing else in python

--- dayFour/python/transcation_log_app.py
+++ dayFour/python/transcation_log_app.py
@@ -1,15 +1,29 @@
 
-balance = 0
+account_balance = 0
 amount = 0
+transaction = "no transaction yet"
+
+account = [amount, account_balance, transaction]
+updated = account
+
 
 def getBalance():
-    return balance
+    return updated[1];
+    
+
 
 
 def deposit(newAmount):
-    amount = newAmount
-    newBalance = newAmount + getBalance()
-    balance = newBalance
+    updated[0] = newAmount
+    updated[1] = newAmount + account_balance
+    updated[2] = "Deposited: " + str(newAmount) + " | New Balance " + str(updated[1])
+    
+amount = 1000
+
+deposit(amount);
+
+print(updated)
+
 
 #def transactionHistory():
 #    reply = "Deposited: #" + amount + " | New Balance: #" + balance
