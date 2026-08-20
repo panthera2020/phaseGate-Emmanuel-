7b917dc64c3062f4e642ee393c031c42dd88ff8c
panthera2020 <diranoye2020@gmail.com>
2026-08-20 14:01:58 +0100

    making correction in python

--- dayFour/python/transcation_log_app.py
+++ dayFour/python/transcation_log_app.py
@@ -5,10 +5,11 @@ transaction = "no transaction yet"
 
 account = [amount, account_balance, transaction]
 updated = account
+transactionHistory = []
 
 
 def getBalance():
-    return updated[1];
+    return updated[2];
     
 
 
@@ -17,13 +18,26 @@ def deposit(newAmount):
     updated[0] = newAmount
     updated[1] = newAmount + account_balance
     updated[2] = "Deposited: " + str(newAmount) + " | New Balance " + str(updated[1])
+    transactionHistory.append(updated[2])
     
-amount = 1000
+amount = 5000
 
 deposit(amount);
 
 print(updated)
 
+def withdraw(newAmount):
+    updated[0] = newAmount;
+    updated[1] = updated[1] - newAmount
+    updated[2] = "Withdrew: " + str(newAmount) + " | New Balance " + str(updated[1])
+    transactionHistory.append(updated[2])
+    
+withdrawalAmount = 2000
+
+withdraw(withdrawalAmount)
+    
+print(updated)
+print(transactionHistory)
 
 #def transactionHistory():
 #    reply = "Deposited: #" + amount + " | New Balance: #" + balance
