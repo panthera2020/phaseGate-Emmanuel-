58c1b7a611f69012db24c93d5eed025762d07627
panthera2020 <diranoye2020@gmail.com>
2026-08-25 14:24:38 +0100

    completed the functions needed

---
+++ dayNine/Java/MiniParkingFunctions.java
@@ -0,0 +1,39 @@
+public class MiniParkingFunctions {
+    private int [] parkingSpace;
+    private String [][] spaceAvailability;
+    
+    public MiniParkingFunctions(){
+        int [] parkingSpace = new int [20];
+        String [][] spaceAvailability = {{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"},{"Empty","Parked"}};
+        
+        this.parkingSpace = parkingSpace;
+        this.spaceAvailability = spaceAvailability; 
+    }
+    
+    
+    public String[] checkParkingSpace(){
+        String [] slotAvailable = new String[20];
+        for(int index = 0; index < parkingSpace.length; index++){
+            if(parkingSpace[index] == 0){
+                slotAvailable[index] = spaceAvailability[index][0];
+            }
+            if(parkingSpace[index] == 1){
+                slotAvailable[index] = spaceAvailability[index][1];
+            }
+        }
+        
+    return slotAvailable;
+    }
+    
+    public void chooseSlot(int slot){
+        if(parkingSpace[slot - 1] == 0){
+            this.parkingSpace[slot - 1]++;
+        }
+    }
+    
+    public void checkOut(int slot){
+        if(parkingSpace[slot - 1] == 1){
+            this.parkingSpace[slot - 1]--;
+        }
+    }
+}
---
+++ dayNine/Java/TestMiniParkingFunctions.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMiniParkingFunctions {
+
+    @Test
+    void TestThatWhenICheckTheParkingSpaceIGetAllTheParkingSpaceEmpty(){
+        MiniParkingFunctions park = new MiniParkingFunctions();
+        
+        //When
+        String [] actualSlotAvailable = park.checkParkingSpace();
+        String [] expectedSlotAvailable = {"Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty"};
+        
+        //Check
+        assertArrayEquals(expectedSlotAvailable,actualSlotAvailable);
+    }
+    
+    @Test
+    void TestThatWhenISelectedSlotSlotGetFilledAndThatPlaceAndShowsParked(){
+        MiniParkingFunctions park = new MiniParkingFunctions();
+        //Given
+        int slot = 1;
+        park.chooseSlot(slot);
+        
+        //When
+        String [] actualSlotAvailable = park.checkParkingSpace();
+        String [] expectedSlotAvailable = {"Parked","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty"};
+        
+        //Check
+        assertArrayEquals(expectedSlotAvailable,actualSlotAvailable);
+    }
+    
+    @Test
+    void TestThatWhenICheckoutParkingSlotBecomesEmpty(){
+        MiniParkingFunctions park = new MiniParkingFunctions();
+        //Given
+        int slot = 1;
+        park.chooseSlot(slot);
+        
+        String [] actualSlotAvailable = park.checkParkingSpace();
+        String [] expectedSlotAvailable = {"Parked","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty"};
+        
+        assertArrayEquals(expectedSlotAvailable,actualSlotAvailable);
+        
+        //Given
+        int slotTwo = 1;
+        park.checkOut(slot);
+        
+        //When
+        String [] actualSlotNowAvailable = park.checkParkingSpace();
+        String [] expectedSlotNowAvailable = {"Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty","Empty"};
+        
+        //Check
+        assertArrayEquals(expectedSlotNowAvailable,actualSlotNowAvailable);
+        
+        
+    }
+}
